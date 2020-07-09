c821a0452e7371f4a0b8ed18f99f325b4f3b471f
Lasse Skaalum <lskaal19@student.aau.dk>
2020-07-09 18:14:32 +0200

    Mby fixed

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 const bodyParser = require(`body-parser`);
 const fs = require(`fs`);
-const path = require(`path`);
+// const path = require(`path`);
 
 const serve = require(`./server/serveHTML`);
 const mw = require(`./server/middleWare`);
@@ -16,7 +16,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 if (settings.debug) {
   app.use(mw.logger);
 }
-app.use(express.static(`public`));
+app.use(express.static(`${__dirname}/public`));
 
 // Routing
 app.get(`/`, (req, res) => serve(res, `/`));
