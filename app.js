209f080dd30cfdab71edc80a9f69ced1e88bc3d2
Lasse Skaalum <lskaal19@student.aau.dk>
2020-07-09 17:53:46 +0200

    Basic server

--- app.js
+++ app.js
@@ -1,7 +1,32 @@
 const express = require(`express`);
 const app = express();
 const port = 3000;
+const bodyParser = require(`body-parser`);
+const fs = require(`fs`);
 
-app.get(`/`, (req, res) => res.send(`Hello World!`));
+const serve = require(`./server/serveHTML`);
+const pad = require(`./server/HelperFunctions/Pad`);
 
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
+const settings = JSON.parse(fs.readFileSync(`serverSettings.json`));
+
+// Middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+if (settings.debug) {
+  app.use(logger);
+}
+app.use(express.static(`${__dirname}/public/`));
+
+app.get(`/`, (req, res) => serve(res, `/`));
+
+app.listen(port, () => console.log(`skaalum-tech listening at http://localhost:${port}`));
+
+function logger(req, res, next) {
+  const reqMethod = pad(req.method, -6, ` `);
+  const reqUrl    = `${req.protocol}://${req.get(`host`)}${req.originalUrl}`;
+  const now       = new Date();
+  const date      = `${pad(now.getDate(), -2, `0`)}/${pad(now.getMonth(), -2, `0`)}/${now.getFullYear()}`;
+  const time      = `${pad(now.getHours(), -2, `0`)}:${pad(now.getMinutes(), -2, `0`)}:${pad(now.getSeconds(), -2, `0`)}`;
+  console.log(`${date} ${time} - GOT ${reqMethod}: ${reqUrl}`);
+  next();
+}
---
+++ server/serveHTML.js
@@ -0,0 +1,10 @@
+const path = require(`path`);
+
+module.exports = (res, route) => {
+  const root = path.join(__dirname, `../`);
+
+  switch (route) {
+    case `/`: return res.sendFile(path.join(root, `/public/index.html`));
+    default: return res.sendStatus(404);
+  }
+};
