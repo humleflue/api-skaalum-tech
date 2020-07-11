2c163a57459f6853c78019bc3ef61fbb2975627d
Lasse Skaalum <lskaal19@student.aau.dk>
2020-07-11 19:18:26 +0200

    removed console.log

--- server/error_handler.js
+++ server/error_handler.js
@@ -6,15 +6,14 @@ const settings = JSON.parse(fs.readFileSync(`server_settings.json`));
 
 module.exports = (express, app) => {
   app.use((err, req, res, next) => { // eslint-disable-line no-unused-vars
+    if (settings.debug) {
+      console.error(err);
+    }
+
     let response = {};
     let statusCode = 500;
     // The RegEx reads everything until the first colon (or whitespace) because SQL-error-messages looks like this: "ERROR_CODE: Bla bla.... "
     const errorType = err.code || /^[^:^ ]+/.exec(err.message)[0];
-
-    if (settings.debug) {
-      console.error(err);
-    }
-    console.log(errorType);
 
     switch (errorType) {
       case `SAMPLE_ERROR`:
