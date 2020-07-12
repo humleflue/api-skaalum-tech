80d4d6d3e99ff613ff9f3267c6f2e58d2bc48b94
Lasse Skaalum <lskaal19@student.aau.dk>
2020-07-12 17:34:32 +0200

    Hopefully you can now remove logs on the server too

--- server/logs/rmlogs.js
+++ server/logs/rmlogs.js
@@ -3,6 +3,7 @@
 const rimraf = require(`rimraf`);
 const path = require(`path`);
 
+// Removes all .log files in this directory
 rimraf(path.join(__dirname, `*.log`), (err) => {
   if (err) {
     throw err;
