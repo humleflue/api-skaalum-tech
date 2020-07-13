d6f65ed4b84279bc1d2e1be0f265c4443efa7051
Lasse Skaalum <lskaal19@student.aau.dk>
2020-07-13 10:47:45 +0200

    Might have fixed log by updating time on each call of logFile

--- server/helper_functions/consol_log_file.js
+++ server/helper_functions/consol_log_file.js
@@ -7,11 +7,10 @@ const Time = require(`./Time`);
 
 // Modifies the console, such that it logs into the server/logs dir
 module.exports = () => {
-  const time = new Time();
   const logDir = path.join(__dirname, `..`, `logs`);
-
   const logStdout = process.stdout;
   const logFile = (log) => {
+    const time = new Time();
     fs.appendFile(path.join(logDir, `${time.dashUSDate}.log`), log, (error) => {
       if (error) {
         throw error;
